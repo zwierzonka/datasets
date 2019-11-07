9ba97e35bf52a1ab2f961bdce7617b0347f7bb24
Robert Mroczkowski <robert.mroczkowski@allegro.pl>
2019-11-07 15:08:57 +0100

    download dataset

--- scripts/run_dataflow.sh
+++ scripts/run_dataflow.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-DATASET_NAME=wikipedia/20191101.pl
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: scripts/run_dataflow.sh dataset_config"
+    echo "Example: scripts/run_dataflow.sh wikipedia/20191101.pl"
+    echo "Example: scripts/run_dataflow.sh c4/pl"
+fi
+
+DATASET_NAME=$1
 GCP_PROJECT=sc-8395-kraken-prod
 GCP_BUCKET=gs://polish_language_model_data
 GCP_REGION=europe-west1
